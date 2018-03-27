57bf59bd5ff3b51f6f6c0ad91ac5cc285eaaf783
mangster80 <magnus.wallin@student.kyh.se>
2018-03-27 15:25:21 +0200

    testing react routs

--- src/index.js
+++ src/index.js
@@ -2,8 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import Login from './Login'
+import Classes from './Classes'
 import 'bootstrap/dist/css/bootstrap.css';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+  <BrowserRouter>
+    <div>
+      <Route exact path='/' component={App}/>
+      <Route path='/Login' component={Login}/>
+      <Route path='/Classes' component={Classes}/>
+    </div>
+  </BrowserRouter>,
+  document.getElementById('root')
+)
